256cbdeab68c87714fb7d4070981c1dd51e1c194
ajdshdl <92495123+ajdshdl@users.noreply.github.com>
2022-08-17 20:10:10 +0900

    start learning typescript
    zzzz

---
+++ index.ts
@@ -0,0 +1,24 @@
+let 링크 = document.querySelector('.link');
+if (링크 instanceof HTMLAnchorElement){
+    링크.href = 'https://kakao.com';
+}
+let 버튼 = document.querySelector('#button');
+버튼?.addEventListener('click',function(){
+    
+})
+// let 제목 = document.querySelector('#title') as Element;
+// 제목.innerHTML = '반가워요';
+
+// if (제목?.innerHTML){   // ?  없으면 undefined   (optional chaining)
+//     제목.innerHTML ='반가워요';
+// }
+
+//셀렉터로 찾은것은 널일가능성이있기때문에 내로잉(narrowing)함
+
+// if (제목 instanceof Element){
+//     제목.innerHTML ='반가워요'
+// }
+
+// if (제목 != null){
+//     제목.innerHTML ='반가워요'
+// }
